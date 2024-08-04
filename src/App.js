7ea2a7678e0b8dbe844433c119b6d4f686242611
AnastasiaTshnkva <tsuhankova.anastasia@gmail.com>
2024-08-04 13:58:18 +0300

    added some styles and posts components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import '../src/styles/app.css';
+import PostsList from './components/PostList';
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('some text');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaSctipt', body: 'Description'},
+    {id: 2, title: 'JavaSctipt 2', body: 'Description'},
+    {id: 3, title: 'JavaSctipt 3', body: 'Description'},
+  ]);
 
-  const incrementFunc = () => setLikes(likes + 1);
-  const decrementFunc = () => setLikes(likes - 1);
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ]);
 
   return (
-    <div>
-      <h1>{value}</h1>
+    <div className='App'>
+      <PostsList posts={posts} title='JS posts'/>
+      <PostsList posts={posts2} title='Python posts'/>
+
+
+      {/* <h1>{value}</h1>
       <input 
         type='text' 
         value={value}
         onChange={event => setValue(event.target.value)}
       />
       <Counter/>
-      <ClassCounter/>
+      <ClassCounter/> */}
     </div>
   );
 }
